0fb0240fdddbab890a60fbf01672bf186c58dba9
Kurt Mueller <mueller.128@osu.edu>
2016-03-23 19:00:31 -0400

    Create CopyVertexYsToHeights function to copy mesh vertices’ y values into 2d heights array

--- Assets/shallow_wave.cs
+++ Assets/shallow_wave.cs
@@ -1,53 +1,86 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class shallow_wave : MonoBehaviour {
+public class shallow_wave : MonoBehaviour
+{
 	int size;
+	// previous height field of the mesh
+	float[,] old_h;
+	// current height field of the mesh
+	float[,] h;
+	// new height field of the mesh
+	float[,] new_h;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		size = 64;
+		old_h = new float[size, size];
+		h = new float[size, size];
+		new_h = new float[size, size];
 	
 		//Resize the mesh into a size*size grid
 		Mesh mesh = GetComponent<MeshFilter> ().mesh;
 		mesh.Clear ();
-		Vector3[] vertices=new Vector3[size*size];
-		for (int i=0; i<size; i++)
-		for (int j=0; j<size; j++) 
-		{
-			vertices[i*size+j].x=i*0.2f-size*0.1f;
-			vertices[i*size+j].y=0;
-			vertices[i*size+j].z=j*0.2f-size*0.1f;
-		}
+		Vector3[] vertices = new Vector3[size * size];
+		for (int i = 0; i < size; i++)
+			for (int j = 0; j < size; j++) {
+				vertices [i * size + j].x = i * 0.2f - size * 0.1f;
+				vertices [i * size + j].y = 0;
+				vertices [i * size + j].z = j * 0.2f - size * 0.1f;
+			}
 		int[] triangles = new int[(size - 1) * (size - 1) * 6];
 		int index = 0;
-		for (int i=0; i<size-1; i++)
-		for (int j=0; j<size-1; j++)
-		{
-			triangles[index*6+0]=(i+0)*size+(j+0);
-			triangles[index*6+1]=(i+0)*size+(j+1);
-			triangles[index*6+2]=(i+1)*size+(j+1);
-			triangles[index*6+3]=(i+0)*size+(j+0);
-			triangles[index*6+4]=(i+1)*size+(j+1);
-			triangles[index*6+5]=(i+1)*size+(j+0);
-			index++;
-		}
+		for (int i = 0; i < size - 1; i++)
+			for (int j = 0; j < size - 1; j++) {
+				triangles [index * 6 + 0] = (i + 0) * size + (j + 0);
+				triangles [index * 6 + 1] = (i + 0) * size + (j + 1);
+				triangles [index * 6 + 2] = (i + 1) * size + (j + 1);
+				triangles [index * 6 + 3] = (i + 0) * size + (j + 0);
+				triangles [index * 6 + 4] = (i + 1) * size + (j + 1);
+				triangles [index * 6 + 5] = (i + 1) * size + (j + 0);
+				index++;
+			}
 		mesh.vertices = vertices;
 		mesh.triangles = triangles;
 		mesh.RecalculateNormals ();
 	}
 
-	void Shallow_Wave()
+	void Shallow_Wave ()
 	{	
 		float rate = 0.005f;
 		float damping = 0.999f;
 	}
 
+	float[,] CopyVertexYsToHeights (Vector3[] vertices)
+	{
+		int length = vertices.Length;
+		int side = (int)Math.Sqrt ((double)length);
+
+		float[,] heights = new float[side, size];
+
+		int vertexIndex = 0;
+
+		for (int y = 0; y < side; y++) {
+			for (int x = 0; x < side; x++) {
+				heights [y, x] = vertices [vertexIndex].y;
+
+				vertexIndex++;
+			}
+		}
+
+		return heights;
+	}
+
 	// Update is called once per frame
-	void Update () 
+	void Update ()
 	{
 		Mesh mesh = GetComponent<MeshFilter> ().mesh;
 		Vector3[] vertices = mesh.vertices;
+
+		float[,] heights = CopyVertexYsToHeights (vertices);
+
 
 		//Step 1: Copy vertices.y into h
 
