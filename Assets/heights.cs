db173a61124577594ae7ef2a8e2d78f7b78ac09e
Kurt Mueller <kurtronaldmueller@gmail.com>
2016-03-23 21:52:34 -0400

    Fix heights util class

--- Assets/heights.cs
+++ Assets/heights.cs
@@ -6,9 +6,9 @@ public class Heights
     {
     }
 
-    public static float[,] CopyYsToHeights(float[] ys)
+    public static float[,] Ys_To_Heights(float[] ys)
     {
-        int side = (int)Math.Log((double) ys.Length);
+        int side = (int)Math.Sqrt((double) ys.Length);
         float[,] heights = new float[side, side];
 
         int yIndex = 0;
@@ -23,5 +23,27 @@ public class Heights
         }
 
         return heights;
+    }
+
+    public static float[] Heights_To_Ys(float[,] heights)
+    {
+        int length = heights.Length;
+        int side = (int)Math.Sqrt((double) length);
+
+        float[] ys = new float[length];
+
+        int yIndex = 0;
+
+        for(int y = 0; y < side; y++)
+        {
+            for(int x = 0; x < side; x++)
+            {
+                ys[yIndex] = heights[y, x];
+
+                yIndex++;
+            }
+        }
+
+        return ys;
     }
 }
