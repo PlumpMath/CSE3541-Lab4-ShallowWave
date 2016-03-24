057b3489b8cf678388cd5202ced2beabfd6dcc99
Kurt Mueller <kurtronaldmueller@gmail.com>
2016-03-23 21:52:07 -0400

    Add unit tests

---
+++ ShallowWaveTests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShallowWaveTests
+{
+    [TestClass]
+    public class HeightsTests
+    {
+        [TestMethod]
+        public void CopyYsToHeightsTests()
+        {
+            float[] ys = new float[9] {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f};
+            float[,] expectedHeights = new float[,] { { 1.0f, 2.0f, 3.0f }, 
+                                                      { 4.0f, 5.0f, 6.0f }, 
+                                                      { 7.0f, 8.0f, 9.0f } };
+
+            float[,] actualHeights = Heights.Ys_To_Heights(ys);
+
+            CollectionAssert.AreEqual(expectedHeights, actualHeights);
+        }
+
+        [TestMethod]
+        public void CopyHeightsToYsTests()
+        {
+            float[] expectedYs = new float[9] {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f};
+
+            float[,] heights = new float[,] { { 1.0f, 2.0f, 3.0f }, 
+                                              { 4.0f, 5.0f, 6.0f }, 
+                                              { 7.0f, 8.0f, 9.0f } };
+
+            float[] actualYs = Heights.HeightsToYs(heights);
+
+            CollectionAssert.AreEqual(expectedYs, actualYs);
+        }
+    }
+}
