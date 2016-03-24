04e98de86b16fa0d8859f1a93cf4e49fdff3c1be
Kurt Mueller <kurtronaldmueller@gmail.com>
2016-03-23 22:32:28 -0400

    Add Heights.From_Vertices and Heights.To_Vertex_Ys class methods

--- Assets/heights.cs
+++ Assets/heights.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using UnityEngine;
+using System;
 
 public class Heights
 {
@@ -46,4 +47,47 @@ public class Heights
 
         return ys;
     }
+
+    public static float[,] From_Vertices(Vector3[] vs)
+    {
+        float[] ys = Vertices_To_Ys(vs);
+        float[,] heights = From_Ys(ys);
+
+        return heights;
+    }
+
+    public static Vector3[] To_Vertex_Ys(float[,] heights, Vector3[] vs)
+    {
+        float[] ys = To_Ys(heights);
+        Vector3[] newVs = Ys_To_Vertex_Ys(ys, vs);
+
+        return newVs;
+    }
+
+    private static float[] Vertices_To_Ys(Vector3[] vs)
+    {
+        int length = vs.Length;
+        float[] ys = new float[length];
+
+        for(int i = 0; i < length; i++)
+        {
+            ys[i] = vs[i].y;
+        }
+
+        return ys;
+    }
+
+    private static Vector3[] Ys_To_Vertex_Ys(float[] ys, Vector3[] vs)
+    {
+        int length = ys.Length;
+
+        for(int i = 0; i < length; i++)
+        {
+            vs[i].y = ys[i];
+
+        }
+
+        return vs;
+    }
+
 }
--- ShallowWaveTests/ShallowWaveTests.cs
+++ ShallowWaveTests/ShallowWaveTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEngine;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ShallowWaveTests
@@ -7,7 +8,7 @@ namespace ShallowWaveTests
     public class HeightsTests
     {
         [TestMethod]
-        public void CopyYsToHeightsTests()
+        public void FromYsTests()
         {
             float[] ys = new float[9] {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f};
             float[,] expectedHeights = new float[,] { { 1.0f, 2.0f, 3.0f }, 
@@ -20,7 +21,7 @@ namespace ShallowWaveTests
         }
 
         [TestMethod]
-        public void CopyHeightsToYsTests()
+        public void ToYsTests()
         {
             float[] expectedYs = new float[9] {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f};
 
@@ -31,6 +32,44 @@ namespace ShallowWaveTests
             float[] actualYs = Heights.To_Ys(heights);
 
             CollectionAssert.AreEqual(expectedYs, actualYs);
+        }
+
+        [TestMethod]
+        public void FromVerticesTests()
+        {
+            Vector3[] vertices = new Vector3[] { new Vector3(0.0f, 0.0f, 0.0f),
+                                                 new Vector3(0.0f, 1.0f, 0.0f),
+                                                 new Vector3(0.0f, 2.0f, 0.0f),
+                                                 new Vector3(0.0f, 3.0f, 0.0f) };
+
+            float[,] expectedHeights = { { 0.0f, 1.0f },
+                                         { 2.0f, 3.0f } };
+
+            float[,] actualHeights = Heights.From_Vertices(vertices);
+
+            CollectionAssert.AreEqual(expectedHeights, actualHeights);
+        }
+
+
+        [TestMethod]
+        public void ToVertexYsTests()
+        {
+            Vector3[] vertices = new Vector3[] { new Vector3(0.0f, 0.0f, 0.0f),
+                                                 new Vector3(0.0f, 0.0f, 0.0f),
+                                                 new Vector3(0.0f, 0.0f, 0.0f),
+                                                 new Vector3(0.0f, 0.0f, 0.0f) };
+
+            float[,] heights = { { 0.0f, 1.0f },
+                                 { 2.0f, 3.0f } };
+
+            Vector3[] expectedVs = new Vector3[] { new Vector3(0.0f, 0.0f, 0.0f),
+                                                   new Vector3(0.0f, 1.0f, 0.0f),
+                                                   new Vector3(0.0f, 2.0f, 0.0f),
+                                                   new Vector3(0.0f, 3.0f, 0.0f) };
+
+            Vector3[] actualVs = Heights.To_Vertex_Ys(heights, vertices);
+
+            CollectionAssert.AreEqual(expectedVs, actualVs);
         }
     }
 }
